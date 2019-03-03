16e2e03d4e16999d98b56ae5bc6db6af1270a073
Grace <gracetaira03@gmail.com>
2019-03-03 13:03:52 -0800

    Fixed elevator error
    oops?

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -69,7 +69,7 @@ public class Robot extends TimedRobot{
         /*
                 [ROBOT DRIVING]
          */
-        robotMap.manipulatorA.setSpeed(OI.manipulatorContoller.getY()); //Dummy manipulator  (uses gamepad)
+        robotMap.elevator.setSpeed(OI.manipulatorContoller.getY()); //Dummy manipulator  (uses gamepad)
         m_drive.arcadeDrive((-OI.driveJoystick.getY()),(OI.driveJoystick.getX())); //Drives the robot arcade style using the joystick
         //We suspect that there may be an issue with the Joystick, b/c it is inverted/reversed. We resolved this by flipping Y,X to X,Y and putting a negative on Y.
 
