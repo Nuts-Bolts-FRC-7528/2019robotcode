31ad2e00508415590e06ee76157905b0fc6edb4a
RiceBowl1234 <47127433+RiceBowl1234@users.noreply.github.com>
2019-03-18 16:26:59 -0700

    maxMotorSpeed and output to control max motor level

--- src/main/java/frc/robot/components/cargoSync.java
+++ src/main/java/frc/robot/components/cargoSync.java
@@ -4,58 +4,67 @@ import frc.robot.common.OI;
 import frc.robot.common.robotMap;
 
 public class cargoSync {
+    private static double P = 0.1;
+    private static double I = 0.0;
+    private static double D = 0.0;
+    public static void go() {
+    robotMap.cargoPivitOne.setSpeed(drive(output1(robotMap.encoderPivitOne.getRate(), robotMap.encoderPivitTwo.getRate()),max()));
+    robotMap.cargoPivitTwo.setSpeed(drive(output2(robotMap.encoderPivitOne.getRate(), robotMap.encoderPivitTwo.getRate()), max()));
+
+}
     //the motor equation is roughly equal to percentSpeed * (AnalogInput + syncOutput + setPosOutput)
-    public static double targetDistanceOrCount() {
+    private static double targetDistanceOrCount() {
 
             return 1.0;
 }
-    public static void go() {
-        robotMap.cargoPivitOne.setSpeed(
-                //desiredMax motor speed
-                percentSpeed() *
-                 (
-                         OI.manipulatorController.getY()
-                                 //output of motor sync PID
-                                 + motor1(
-                                         robotMap.encoderPivitOne.getRate(),
-                                 robotMap.encoderPivitTwo.getRate()
-                         )
-                + moveCargo(targetDistanceOrCount(), robotMap.encoderPivitOne.get()))
-        );
-        robotMap.cargoPivitTwo.setSpeed(
-                percentSpeed() *
-                        (
-                                OI.manipulatorController.getY()
-                //output of motor sync PID
-                 + motor2(
-                         robotMap.encoderPivitOne.getRate(),
-                                        robotMap.encoderPivitTwo.getRate()
-                                )
-                                + moveCargo(targetDistanceOrCount(), robotMap.encoderPivitTwo.get())
-                        )
-        );
+private static double max() {
+        return 0.8;
+    }
+private static double drive( double output, double maxMotorSpeed) {
+
+        if (output < maxMotorSpeed)
+            output = maxMotorSpeed;
+        return output;
+
+}
+
+
+    private static double output1(double encoder1, double encoder2) {
+            return  percentSpeed() *
+                    (OI.manipulatorController.getY()
+                                    //output of motor sync PID
+                                    + motor1(encoder1, encoder2)
+                                    + moveCargo(targetDistanceOrCount(), encoder1)
+                    );
+
+    }
+    private static double output2(double encoder1, double encoder2) {
+        return  percentSpeed() *
+                (OI.manipulatorController.getY()
+                                //output of motor sync PID
+                                + motor2(encoder1, encoder2)
+                                + moveCargo(targetDistanceOrCount(), encoder1)
+                );
+
     }
 
 
 
 
-    public static double motor1(double encoder1, double encoder2) {
-        double P1 = 0.1;
-        double I1 = 0.0;
-        double D1 = 0.0;
+
+    private static double motor1(double encoder1, double encoder2) {
+
         double pv1 = (robotMap.encoderPivitOne.getRate() + robotMap.encoderPivitTwo.getRate()) / 2;
-        double output = pidMethod(P1, I1, D1, RLPC(), pv1) + pidMethod(P1, I1, D1, encoder2, encoder1);
+        double output = pidMethod(P, I, D, RLPC(), pv1) + pidMethod(P, I, D, encoder2, encoder1);
         return output;
     }
-    public static double motor2 (double encoder1, double encoder2) {
-        double P2 = 0.1;
-        double I2 = 0.0;
-        double D2 = 0.0;
+    private static double motor2 (double encoder1, double encoder2) {
+
         double pv2 = (robotMap.encoderPivitOne.getRate() + robotMap.encoderPivitTwo.getRate()) / 2;
-        double output = pidMethod(P2, I2, D2, RLPC(), pv2) + pidMethod(P2, I2, D2, encoder1, encoder2);
+        double output = pidMethod(P, I, D, RLPC(), pv2) + pidMethod(P, I, D, encoder1, encoder2);
         return output;
     }
-    public static double percentSpeed() {
+    private static double percentSpeed() {
         return 0.2; // % motor speed recommended max .9
     }
 
@@ -72,15 +81,15 @@ public class cargoSync {
 
 
         error = sp - pv;
-        integral += error;
+        integral += error * .02;
         if (integral < - integralLimit)
             integral = -integralLimit;
         if (integral > integralLimit)
             integral = integralLimit;
-        derivative = error - previous_error;
+        derivative = (error - previous_error)/.02;
         previous_error = error;
         output = P * error + I * integral + D * derivative;
-        return output;
+        return output/100.0;
 
 
 
@@ -89,7 +98,7 @@ public class cargoSync {
 
 
 
-    public static double RLPC() {
+    private static double RLPC() {
         double change; // math
 
 
@@ -110,144 +119,9 @@ public class cargoSync {
         RLPC +=change;
         return RLPC;
     }
-    public static double moveCargo(double targetcount, double encoder1) {
-        double P = 0.1;
-        double I = 0.0;
-        double D = 0.0;
+    private static double moveCargo(double targetcount, double encoder1) {
+
 
         return pidMethod(P,I,D, targetcount, encoder1);
     }
-
-
-
-/*
-                [ROBOT CARGO-PICKUP]
-         */
-
-    //I reallly really hope this works - By Kobe Nguyen
-
-    ////////////////////////////////////////////////////////////////////////////////
-
-
-
-    //These variables are part of the math i.e. no touch!
-    double error1a;
-    double error1ai = 0;
-    double error1ad = 0;
-    double previous_error1a = 0;
-    double cmd1a;
-
-    double error1b;
-    double error1bi = 0;
-    double error1bd = 0;
-    double previous_error1b = 0;
-    double cmd1b;
-
-    double error2a;
-    double error2ai = 0;
-    double error2ad = 0;
-    double previous_error2a = 0;
-    double cmd2a;
-
-    double error2b;
-    double error2bi = 0;
-    double error2bd = 0;
-    double previous_error2b = 0;
-    double cmd2b;
-
-
-    //The calculated (output) in getRate() which the motors should be run at.
-    double cmd1;  // motor1
-    double cmd2;   // motor2
-
-
-
-    // tuning constants:
-    double i1aLimit = 1;     // integrator clamp for M1 a
-    double i1bLimit = 1;     // integrator clamp for M1 b
-
-    double i2aLimit = 1;   // integrator clamping value for M2 a
-    double i2bLimit = 1;   // integrator clamping value for M2 b
-
-    // Theoretically, the gains should all share the same constant, so if you want, you should change them to 1 variable
-
-    double Kp1a = 1;  // PID gains 1a
-    double Ki1a = 0;
-    double Kd1a = 0;
-
-    double Kp1b = 1;  // PID gains 1b
-    double Ki1b = 0;
-    double Kd1b = 0;
-
-
-    double Kp2a = 1; // PID gains 2a
-    double Ki2a = 0;
-    double Kd2a = 0;
-
-
-    double Kp2b = 1; // PID gains 2b
-    double Ki2b = 0;
-    double Kd2b = 0;
-
-
-// this is your control loop:
-/*
-
-
-// PID controller for M1
-
-    //part1 Motor1 PID //see Ether's graphic and Java PID wikipedia article
-
-    error1a =RLPC -(e1pos +e2pos)/2; //closed loop error
-    error1ai +=error1a; // integrate the error
-        if(error1ai >i1aLimit)error1ai =i1aLimit; //clamp the integrated error
-        else if(error1ai< -i1aLimit)error1ai =-i1aLimit;
-    error1a =error1a -previous_error1a; //rate of change in error1 for D term
-    previous_error1a =error1a; // save for next iteration
-    cmd1a =Kp1a *error1a +Ki1a *error1ai +Kd1a *error1ad;
-
-    //part2 Motor 1 PID partial master/slave input
-    error1b =e2pos -e1pos; //closed loop error
-    error1bi +=error1b; // integrate the error
-        if(error1bi >i1aLimit)error1bi =i1bLimit; // clamp the integrated error
-        else if(error1bi< -i1bLimit)error1bi =-i1bLimit;
-    error1bd =error1b -previous_error1a; // rate of change in error1a for D tern
-    previous_error1a =error1b; //save for next iteration
-    cmd1b =Kp1b *error1b +Ki1b *error1bi +Kd1b *error1bd;
-
-    cmd1 =0.9*(cmd1a +cmd1b); // limit to 90% b/c 100% motor power is always different
-// PID controller for M2
-
-    //part1 motor1
-    error2a =RLPC -(e1pos +e2pos)/2;//closed loop error
-    error2ai +=error2a;// integrate the error
-        if(error2ai >i2aLimit)error2ai =i2aLimit;// clamp the integrated error
-        else if(error2ai< -i2aLimit)error2ai =-i2aLimit;
-    error2a =error2a -previous_error2a;// rate of change in error1a for D tern
-    previous_error2a =error2a;//save for next iteration
-    cmd2a =Kp2a *error2a +Ki2a *error2ai +Kd2a *error2ad;
-
-    //part 2 Motor2
-    error2b =e1pos -e2pos;//closed loop error
-    error2bi +=error2b;// integrate the error
-        if(error2bi >i2bLimit)error2bi =i2bLimit;// clamp the integrated error
-        else if(error2bi< -i2bLimit)error2bi =-i2bLimit;
-    error2b =error2b -previous_error2b;// rate of change in error1a for D tern
-    previous_error2b =error2a;//save for next iteration
-    cmd2b =Kp2b *error2b +Ki2b *error2bi +Kd2b *error2bd;
-
-    cmd2 =0.9*(cmd2a +cmd2b); // limit to 90% b/c 100% motor power is ALWAYS different
-
-    ///////////////////////////////////////////////////
-    double howfast = .2; // constant for base motor speed.
-    // Ideally, the output of the PID should ADD onto baseSpeed,
-    // however, I am not sure sure, more like kinda sure, but not really.
-    // Just know that it also happens to be a double :D
-
-        robotMap.cargoPivitTwo.setSpeed(OI.manipulatorContoller.getY()*howfast+cmd1);
-        robotMap.cargoPivitOne.setSpeed(OI.manipulatorContoller.getY()*howfast+cmd2);
-
-
-/////////////////////////////////////////////////////////
-    */
 }
