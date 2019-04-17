bb8d2c2b47639784faa2db56dfc7e8ba6a1fb6ed
Ethan Hanlon <67823@students.mhusd.org>
2019-04-16 22:38:31 -0700

    commented out print statements

--- src/main/java/frc/robot/components/Elevator.java
+++ src/main/java/frc/robot/components/Elevator.java
@@ -69,17 +69,17 @@ public class Elevator {
         }
 
 //        Print methods
-        System.out.println("\n\n*******************************");
-        System.out.println("\nElevator drive:  " + drive);
-        System.out.println("\nElevator is at:  " + robotMap.elevatorEncoder.get());
-        System.out.println("\nElevator Setpoint:  " + setpoint);
-        System.out.println("\nElevator Goal:  " + goal);
-        System.out.println("\nElevator " + robotMap.elevator.getMotorOutputPercent());
-        if (hatchOrCargo) {
-            System.out.println("BALL        BALL");
-        } else {
-            System.out.println("HATCH       HATCH");
-        }
+//        System.out.println("\n\n*******************************");
+//        System.out.println("\nElevator drive:  " + drive);
+//        System.out.println("\nElevator is at:  " + robotMap.elevatorEncoder.get());
+//        System.out.println("\nElevator Setpoint:  " + setpoint);
+//        System.out.println("\nElevator Goal:  " + goal);
+//        System.out.println("\nElevator " + robotMap.elevator.getMotorOutputPercent());
+//        if (hatchOrCargo) {
+//            System.out.println("BALL        BALL");
+//        } else {
+//            System.out.println("HATCH       HATCH");
+//        }
 //        END of print methods
     }
 
